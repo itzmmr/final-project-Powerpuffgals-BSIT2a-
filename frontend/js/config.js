32a1b27adabb784a5daba446694632bb8ee23540
ivyangel-hidalgo <ivyangelrivera.hidalgo24@bicol-u.edu.ph>
2026-05-11 05:12:25 +0800

    Added NEXUSWrites URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:5000/api'
-    : 'https://final-project-backend.onrender.com/api';  // ← your real backend URL
+    : 'https://final-project-powerpuffgals-bsit2a.onrender.com';  // ← your real backend URL
 
 export default API_BASE_URL;
